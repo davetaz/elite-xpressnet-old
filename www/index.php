2e99968880c5f6b26604b250a970d1ec32e7521d
Dave Tarrant <davetaz@gmail.com>
2013-06-07 08:06:51 +0100

    Styled web interface, this will break the feedback from the Elite DCC for now

---
+++ www/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DCC xPressNet Control</title>
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/rail.css" />
+	<link rel="stylesheet" href="css/jquery.mobile-1.3.1.min.css" />
+	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery.mobile-1.3.1-davetaz.js"></script>
+	<script src="js/loco_status.js"></script>
+</head>
+<body>
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>xPressNet Control</h1>
+		<a href="#" id="loco-chooser" data-role="button" data-icon="plus" data-iconpos="left" data-mini="true" data-inline="true" class="ui-btn-right">Add Loco</a>
+	</div>
+
+	<div data-role="content">
+<?php
+	for ($i=3;$i<20;$i++) {
+		draw_loco($i);
+	}
+
+?>
+
+	</div><!-- /content -->
+<!--
+	<div data-role="footer">
+		<h4>&copy; David Tarrant</h4>
+	</div>
+-->
+</div><!-- /page -->
+
+</body>
+</html>
+
+<?php
+
+function draw_loco($id) {
+echo '		<form id="loco_'.$id.'" class="loco">
+		    <label class="loco_label" for="loco-'.$id.'">Loco '.$id.':</label>
+		    <a class="hide" href="#" id="hide_'.$id.'" data-role="button" data-mini="true" data-icon="delete" data-iconpos="notext" class="ui-btn-right"></a>
+		    <input type="range" name="speed-'.$id.'" id="speed-'.$id.'" data-highlight="true" min="0" max="127" value="0">
+		    <div id="direction_panel" class="direction_panel">
+		    	<div data-role="controlgroup" data-type="horizontal">
+				<a href="#" id="reverse_'.$id.'" data-role="button" data-icon="arrow-l" data-iconpos="left" data-mini="true" data-inline="true">Rev.</a>
+				<a href="#" id="forward_'.$id.'" data-role="button" data-icon="arrow-r" data-iconpos="right" data-mini="true" data-inline="true" class="ui-btn-down-c">Fw.</a>
+			</div>
+		    </div>
+		    <div id="function_panel" class="function_panel">
+		    	<label class="function_label" for="F0-'.$id.'">Direction Lights:</label>
+		    	<select name="F0-'.$id.'" id="F0-'.$id.'" data-role="slider" data-mini="true">
+		    		<option value="off">off</option>
+				<option value="on">on</option>
+		    	</select>
+		    </div>
+		</form>';
+}
+?>
+
--- www/js/loco_status.js
+++ www/js/loco_status.js
@@ -1,24 +1,80 @@
+//TODO - Not allow closing of active locos
+//FIXME - Speed, Function and Direction feedback, there is a massive race condition, well done Dave! 
+
+
 $.ajaxSetup ({
     // Disable caching of AJAX responses
     cache: false
 });
 
 $(document).ready(function() {
+	display_3_only();
 	loco_monitor();
+	register_event_handlers();
 });
 
+function display_3_only() {
+	for(i=4;i<128;i++) {
+		$('#loco_'+i).css('display','none');
+	}
+}
+
+function register_event_handlers() {
+	$("a").click(function(event) {
+    		id = this.id;
+		bits = id.split('_');
+		if (bits[0] == "reverse" || bits[0] == "forward") {
+			set_direction(bits[0],bits[1],this);
+		}
+		if (bits[0] == "hide") {
+			node = "loco_" + bits[1];
+			$("#"+node).fadeOut('slow');
+		}
+	});
+}
+
+function set_direction(direction,loco,element) {
+	if (direction == "f") {
+		direction = "forward";
+	}
+	if (direction == "r") {
+		direction = "reverse";
+	}
+	if (element == null) {
+		element = {};
+		element.id = direction + "_" + loco;
+	}
+	opposite_id = "forward_" + loco;
+	if(direction == "forward") {
+		opposite_id = "reverse_" + loco;
+	}
+	if ($('#'+element.id).hasClass('ui-btn-down-c')) {
+	} else {
+		$('#'+element.id).addClass('ui-btn-down-c');
+		$('#'+opposite_id).removeClass('ui-btn-down-c');
+	}
+}
+
 var prev = new Array();
-prev[3] = 0;
+for (i=3;i<128;i++) {
+	prev[i] = 0;
+}
 
 function loco_monitor() {
 	$.ajax('data/locos.json')
 	  .done(function(data) {
-		if (prev[3] != data) {
-			console.log(data);
-			$('#loco-3').val(data);
-			$('#loco-3').slider('refresh');
-			prev[3] = data;
-        	}
+		$.each(data, function(i, obj) {
+			node_id = "loco_" + obj.id;
+			if (!$("#"+node_id).is(':visible') && obj.speed > 0) {
+				$("#"+node_id).fadeIn('slow');
+			}
+			if (prev[obj.id] != obj.speed) {
+				$('#speed-'+obj.id).val(obj.speed);
+				$('#speed-'+obj.id).slider('refresh');
+				prev[obj.id] = 	obj.speed;
+			}
+			set_direction(obj.direction,obj.id,null);	
+		});
 		loco_monitor();
           })
 	  .fail(function() {
