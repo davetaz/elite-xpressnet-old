5f029b9221fddd8a838876c11c06c57721ef0640
Dave Tarrant <davetaz@gmail.com>
2013-09-11 17:25:11 +0100

    Updates to test setup

--- test-scripts/hall.py
+++ test-scripts/hall.py
@@ -1,8 +1,9 @@
 import smbus
 import time
+import sys
 
 bus = smbus.SMBus(1)
-address = 0x20
+addresses = [0x20,0x21]
 
 IODIRA =  0x00 
 IODIRB =  0x01
@@ -17,19 +18,24 @@ INTCAPB=  0x11
 GPIOA  =  0x12
 GPIOB  =  0x13
 
-bus.write_byte_data(address,IODIRA,0xff)
-bus.write_byte_data(address,IODIRB,0x00)
+prev = {}
 
-bus.write_byte_data(address,GPPUA,0xff)
-bus.write_byte_data(address,GPINTENA,0xff)
+for address in addresses:
+	bus.write_byte_data(address,IODIRA,0xff)
+	bus.write_byte_data(address,IODIRB,0x00)
 
-status = bus.read_byte_data(address,0x13)
-print status
-bus.write_byte_data(address,0x13,0)
+	bus.write_byte_data(address,GPPUA,0xff)
+	bus.write_byte_data(address,GPINTENA,0xff)
+
+	status = bus.read_byte_data(address,0x13)
+	bus.write_byte_data(address,0x13,int(status))
+	prev[address]="11111111"
+
 
 while 1 :
-	x=bus.read_byte_data(address,INTFA)
-	if (x & 0x01) : 
-		print 'Sensor 1'
+	for address in addresses:
 		x=bus.read_byte_data(address,INTCAPA)
-		print bin(x)[2:].zfill(8)
+		bar=bin(x)[2:].zfill(8)
+		if (bar != prev[address]) :
+			prev[address] = bar
+			print str(address) + " READING " + str(bar)
--- test-scripts/test.py
+++ test-scripts/test.py
@@ -1,53 +1,41 @@
-import sys
-import serial
+import smbus
 import time
+import sys
 
-ser = serial.Serial('/dev/ttyUSB1',9600)
+bus = smbus.SMBus(1)
+addresses = [0x20,0x21]
 
-def send(message):
-	ok = False
-	trys = 1
-	while (not ok and trys < 5) :
-	  ser.write(message)
-	  print 'trys = %d send:' % (trys) , 
-	  for byte in message:print(hex(byte)) ,
-	  time.sleep(.1)
-	  print ' receive: ',     
-	  while ser.inWaiting() > 0 :
-	  	enq = ser.read()
-	  	print enq.encode('hex') ,  
-	  	if enq == '05'.decode('hex') : 
-		  ok = True
-	  print 
-	  trys += 1
+IODIRA =  0x00 
+IODIRB =  0x01
+GPINTENA = 0X04
+GPINTENB = 0x05
+GPPUA  =  0x0c
+GPPUB  =  0x0d
+INTFA  =  0x0e
+INTFB  =  0x0f
+INTCAPA=  0x10
+INTCAPB=  0x11
+GPIOA  =  0x12
+GPIOB  =  0x13
 
-def parity(message):
-	edb = 0
-	for byte in message: edb ^= byte
-	message.append(edb)
+prev = {}
 
-def setThrottle(address,direction,speed):
-	message = bytearray('E400000000'.decode('hex'))
-	message[1] = 0x13
-	message[3] = address
-	message[4] = speed
-	if direction  == 'f' : message[4] |= int(b'10000000',2)
-	elif direction =='b' : message[4] &= int(b'01111111',2)
-	parity(message) 
-	send(message) 
-#	test(message)
+for address in addresses:
+	bus.write_byte_data(address,IODIRA,0xff)
+	bus.write_byte_data(address,IODIRB,0x00)
 
-def getFirmwareVersion():
-	message = bytearray('212100'.decode('hex'))
-	send(message)
+	bus.write_byte_data(address,GPPUA,0xff)
+	bus.write_byte_data(address,GPINTENA,0xff)
 
-def test(message):
-	for c in message: print(c)
+	status = bus.read_byte_data(address,0x13)
+	bus.write_byte_data(address,0x13,int(status))
+	prev[address]="00000000"
 
 
-print "Loco: " + sys.argv[1]
-print "Direction: " + sys.argv[2]
-print "Speed: " + sys.argv[3]
-#setThrottle(3,"b",0);
-setThrottle(int(sys.argv[1]),sys.argv[2],int(sys.argv[3]));
-#getFirmwareVersion()
+while 1 :
+	for address in addresses:
+		x=bus.read_byte_data(address,INTCAPA)
+		bar=bin(x)[2:].zfill(8)
+		if (bar != prev[address]) :
+			prev[address] = bar
+			print str(address) + " READING " + str(bar)
