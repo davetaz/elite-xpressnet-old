9d12d7517a2b2676eca2e61d9b47947fe2157b76
David Tarrant <davetaz@gmail.com>
2013-10-01 17:16:55 +0100

    test_track.py implements the tests defined in the readme file for the code. This tests the control script is working properly with the example config file

---
+++ redis/test_track.py
@@ -0,0 +1,23 @@
+import redis
+import sys
+from time import sleep
+
+r = redis.Redis()
+
+print "Activting first sensor in section 2 (20,A,0x04), first time"
+r.rpush("sensors","20,A,0x04,1")
+sleep(5)
+print "Activting first sensor in section 2 (20,A,0x04), second time"
+r.rpush("sensors","20,A,0x04,1")
+sleep(5)
+print "Reverse direction of train to face reverse"
+r.rpush("trains","3,Direction,R")
+sleep(5)
+print "Activting first sensor in section 1 (20,A,0x02), first time"
+r.rpush("sensors","20,A,0x02,1")
+sleep(5)
+print "Activting first sensor in section 1 (20,A,0x04), second time"
+r.rpush("sensors","20,A,0x02,1")
+sleep(5)
+print "Reverse direction of train to face forward again"
+r.rpush("trains","3,Direction,F")
