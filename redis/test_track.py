255d81e2f836478c0bad57bdb62ba10929ccf76d
David Tarrant <davetaz@gmail.com>
2013-10-01 17:18:47 +0100

    added docs

--- redis/test_track.py
+++ redis/test_track.py
@@ -1,3 +1,6 @@
+# Overview
+# test_track.py is a stand alone script that implements the tests defined in the readme file for the code. This tests the control script is working properly with the example config file
+
 import redis
 import sys
 from time import sleep
